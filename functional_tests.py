a877847b5db92620c1b8ede1439dfd1adf04f168
Abu H Kamal <kamal3235@hotmail.com>
2024-11-02 03:06:03 -0500

    url config, map / to home_page view

--- functional_tests.py
+++ functional_tests.py
@@ -36,23 +36,25 @@ if __name__ == "__main__":
 
 # Python script checks if it’s been executed from the command line,
 # rather than just imported by another script
+
 # output
-"""
- (.venv) C:\Users\kamal\goat_book>python functional_tests.py
-F
-======================================================================
-FAIL: test_can_start_a_todo_list (__main__.NewVisitorTest.test_can_start_a_todo_list)
-----------------------------------------------------------------------
-Traceback (most recent call last):
-  File "C:\Users\kamal\goat_book\functional_tests.py", line 17, in test_can_start_a_todo_list
-    self.assertIn("To-Do", self.browser.title)
-    ~~~~~~~~~~~~~^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-AssertionError: 'To-Do' not found in 'The install worked successfully! Congratulations!'
 
-----------------------------------------------------------------------
-Ran 1 test in 8.558s
+# """
+#  (.venv) C:\Users\kamal\goat_book>python functional_tests.py
+# F
+# ======================================================================
+# FAIL: test_can_start_a_todo_list (__main__.NewVisitorTest.test_can_start_a_todo_list)
+# ----------------------------------------------------------------------
+# Traceback (most recent call last):
+#   File "C:\Users\kamal\goat_book\functional_tests.py", line 17, in test_can_start_a_todo_list
+#     self.assertIn("To-Do", self.browser.title)
+#     ~~~~~~~~~~~~~^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# AssertionError: 'To-Do' not found in 'The install worked successfully! Congratulations!'
 
-FAILED (failures=1)
+# ----------------------------------------------------------------------
+# Ran 1 test in 8.558s
 
-(.venv) C:\Users\kamal\goat_book>   
-"""
+# FAILED (failures=1)
+
+# (.venv) C:\Users\kamal\goat_book>
+# """
