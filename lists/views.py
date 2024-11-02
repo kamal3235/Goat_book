84fabc58d089943815843dcbbf0934ab2c34754a
Abu H Kamal <kamal3235@hotmail.com>
2024-11-02 13:04:48 -0500

    use dry and refactoring

--- lists/views.py
+++ lists/views.py
@@ -8,5 +8,15 @@ from django.http import HttpResponse
 # def home_page(request):
 #     return HttpResponse("<html><title>To-Do lists</title></html>")
 
+# def home_page(request):
+#     if request.method == "POST":
+#         return HttpResponse("You submitted: " + request.POST["item_text"])
+
+#     return render(request, "home.html")
+
 def home_page(request):
-    return render(request, "home.html")
+    return render(
+        request,
+        "home.html",
+        {"new_item_text": request.POST.get("item_text", "")},
+    )
