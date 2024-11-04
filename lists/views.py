efbcfd46e80ba754b7de51be050ea7b69a36e4c8
Abu H Kamal <kamal3235@hotmail.com>
2024-11-04 02:47:00 -0600

    new URL view for adding to existing lists, FT passes

--- lists/views.py
+++ lists/views.py
@@ -28,18 +28,25 @@ def home_page(request):
     return render(request, "home.html")
 
 
-def view_list(request):
-    items = Item.objects.all()
-    return render(request, "list.html", {"items": items})
+def view_list(request, list_id):
+    our_list = List.objects.get(id=list_id)
+    # items = Item.objects.filter(list=our_list)
+    return render(request, "list.html", {"list": our_list})
 
 
 def new_list(request):
     nulist = List.objects.create()
     Item.objects.create(text=request.POST["item_text"], list=nulist)
-    return redirect("/lists/the-only-list-in-the-world/")
+    return redirect(f"/lists/{nulist.id}/")
 
     # return render(
     #     request,
     #     "home.html",
     #     {"new_item_text": request.POST.get("item_text", "")},
     # )
+
+
+def add_item(request, list_id):
+    our_list = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST["item_text"], list=our_list)
+    return redirect(f"/lists/{our_list.id}/")
--- superlists/urls.py
+++ superlists/urls.py
@@ -24,8 +24,9 @@ from lists.views import home_page
 # ]
 
 urlpatterns = [
-    path("", home_page, name="home"),
+    path("", views.home_page, name="home"),
     path("lists/new", views.new_list, name="new_list"),
-    path("lists/the-only-list-in-the-world/",
+    path("lists/<int:list_id>/",
          views.view_list, name="view_list"),
+    path("lists/<int:list_id>/add_item", views.add_item, name="add_item"),
 ]
